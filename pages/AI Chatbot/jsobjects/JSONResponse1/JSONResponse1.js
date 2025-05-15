00f18ba0e6e8d07ec3a394bc25db768289edeb6d
Jayant Verma <jayantmailac@gmail.com>
2025-05-15 06:19:31 +0000

    System generated commit, initial commit

---
+++ pages/AI Chatbot/jsobjects/JSONResponse1/JSONResponse1.js
@@ -0,0 +1,14 @@
+export default {
+
+	chatHistory: [],
+
+	async sendMessage(){
+		const newMessage = { role: "user", content: Input1.text };
+		this.chatHistory.push(newMessage); 
+		const reply = await Api1.run();
+		this.chatHistory.push({role:"assistant", content: reply.choices[0].message.content});
+		resetWidget('Input1')
+		return this.chatHistory;
+	}
+	
+}
